3d2895f42466bed0cedd342c0f6954bb39793800
s-jad <s_jad@web.de>
2024-01-31 13:12:18 +0100

    Solved day 8 part 1

---
+++ day_8/src/bin/part_1.rs
@@ -0,0 +1,89 @@
+use itertools::Itertools;
+use std::{collections::VecDeque, time::Instant};
+
+fn process(input: &str) -> usize {
+    let len_x = 50;
+    let len_y = 6;
+    let v = vec!['.'; len_x];
+    let row = VecDeque::from(v);
+    let mut grid = Vec::with_capacity(len_y);
+
+    for _ in 0..len_y {
+        grid.push(row.clone());
+    }
+
+    let instructions = input
+        .lines()
+        .map(|instruction| {
+            instruction
+                .split_terminator(&[' ', '\n', '=', 'x', 'y', 'b'])
+                .filter(|s| !s.is_empty())
+                .collect_vec()
+        })
+        .collect_vec();
+
+    for ins in instructions {
+        if ins.len() == 3 {
+            let x = ins[1].parse::<usize>().unwrap();
+            let y = ins[2].parse::<usize>().unwrap();
+
+            for dy in 0..y {
+                for dx in 0..x {
+                    grid[dy][dx] = '#';
+                }
+            }
+        } else {
+            match ins[1] {
+                "row" => {
+                    let yidx = ins[2].parse::<usize>().unwrap();
+                    let amt = ins[3].parse::<usize>().unwrap();
+                    for _ in 0..amt {
+                        let temp = grid[yidx].pop_back().unwrap();
+                        grid[yidx].push_front(temp);
+                    }
+                }
+                "column" => {
+                    let xidx = ins[2].parse::<usize>().unwrap();
+                    let amt = ins[3].parse::<usize>().unwrap();
+
+                    for step in 0..amt {
+                        let mut prev = grid[len_y - 1][xidx];
+                        for y in 0..len_y {
+                            let temp = grid[y][xidx];
+                            grid[y][xidx] = prev;
+                            prev = temp;
+                        }
+                    }
+                }
+                _ => unreachable!(),
+            }
+        }
+    }
+
+    grid.iter()
+        .map(|r| r.iter().filter(|c| c == &&'#').count())
+        .sum()
+}
+
+fn main() {
+    let input = include_str!("../../input.txt");
+
+    let start = Instant::now();
+    let output = process(input);
+    let time = start.elapsed();
+
+    dbg!(output, time);
+}
+
+#[cfg(test)]
+
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_1() {
+        let input = include_str!("../../test.txt");
+        let output = process(input);
+        assert_eq!(result,);
+    }
+}
